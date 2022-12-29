773ebef913647b85caccd4c1f98e4680337c4b94
ali <ali@MacBook-Pro-Ali.local>
2022-12-29 12:51:34 +0400

    mesaj

---
+++ hometask6/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace hometask6;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("ededi daxil et:");
+        int number = int.Parse(Console.ReadLine());
+        int countDigit = 0;
+        int countDigitLog = (int)Math.Log10(number) + 1;
+        int r;
+        int sum;
+        while (number != 0) 
+        {
+            r = number % 10;
+            number /= 10;
+            if (r==0) 
+            {
+                Console.WriteLine(r);
+
+            }countDigit++;
+
+        }
+
+        
+    }
+}
+
+
